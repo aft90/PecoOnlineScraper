1703fb8a4052e2c8961c54cc83881eddff17877f
Cristina Troie <cristina.troie@anre.ro>
2016-12-28 23:47:39 +0200

    Add library to retrieve results

---
+++ ResultsSearch/ResultsSearch.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PecoOnlineScraper.Helpers;
+namespace PecoOnlineScraper.Results
+{
+    public class ResultsSearch
+    {
+        private IWebElement GetResultsTable(IWebDriver driver)
+        {
+            return driver.WaitForElement(By.Id("tabelaRezultate"), 20);
+        }
+
+        public IReadOnlyCollection<double> RetrieveResults(IWebDriver driver)
+        {
+            List<double> results = new List<double>();
+            IWebElement resultsTable = GetResultsTable(driver);
+            var r = resultsTable.FindElements(By.CssSelector(".pretTD"));
+            foreach(var e in r)
+            {
+                results.Add(Double.Parse(e.Text));
+            }
+            return results;
+        }
+    }
+}
