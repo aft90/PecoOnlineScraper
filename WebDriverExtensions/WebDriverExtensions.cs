941bc58156c070956fc56372fe5cebbef4ebc2e1
Cristina Troie <cristina.troie@anre.ro>
2016-12-28 22:21:36 +0200

    Initial setup

---
+++ WebDriverExtensions/WebDriverExtensions.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace PecoOnlineScraper.Helpers
+{
+    public static class WebDriverExtensions
+    {
+        public static IWebElement WaitForElement(this IWebDriver driver, By by, int timeout)
+        {
+           return new WebDriverWait(driver, TimeSpan.FromSeconds(timeout)).Until(ExpectedConditions.ElementIsVisible(by)); 
+        }
+    }
+}
---
+++ WebDriverExtensions/WebDriverFactory.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.PhantomJS;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDriverExtensions
+{
+    public class WebDriverFactory
+    {
+        public static IWebDriver FirefoxWebDriver()
+        {
+            return new FirefoxDriver();
+        }
+
+        public static IWebDriver PhantomJSWebDriver()
+        {
+            var service = PhantomJSDriverService.CreateDefaultService();
+            service.IgnoreSslErrors = true;
+            service.SslProtocol = "any";
+            var driver = new PhantomJSDriver(service);
+            driver.Manage().Window.Size = new System.Drawing.Size(1920, 1080);
+            return driver;
+        }
+    }
+}
