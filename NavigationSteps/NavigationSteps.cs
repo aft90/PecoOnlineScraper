3ae35d3ca0c389dd9ad79fe36c9c316b45850229
Cristina Troie <cristina.troie@anre.ro>
2017-01-03 15:06:40 +0200

    Remove unused imports

--- NavigationSteps/NavigationSteps.cs
+++ NavigationSteps/NavigationSteps.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using PecoOnlineScraper.Helpers;
-using OpenQA.Selenium;
+﻿using OpenQA.Selenium;
 
 namespace PecoOnlineScraper.Steps
 {
--- PecoOnlineScraper/Program.cs
+++ PecoOnlineScraper/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using PecoOnlineScraper.Helpers;
 using PecoOnlineScraper.Steps;
 using PecoOnlineScraper.Results;
