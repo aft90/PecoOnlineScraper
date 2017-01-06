619067b8fdb4b93c979af4f5d54c612533fcbb29
Cristina Troie <cristina.troie@anre.ro>
2017-01-06 11:58:05 +0000

    Add class to hold results & metadata

---
+++ SearchData/SearchData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace PecoOnlineScraper.Data
+{
+    public class SearchData
+    {
+        public SearchData(IDictionary<string, IEnumerable<double>> judetResults)
+        {
+            Metadata = SearchMetadata.NewInstance();
+            JudetResults = judetResults;
+        }
+
+        public SearchMetadata Metadata { get; private set;  }
+        public IDictionary<string, IEnumerable<double>> JudetResults { get; private set; }
+    }
+}
---
+++ SearchData/SearchMetadata.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PecoOnlineScraper.Data
+{
+    public class SearchMetadata
+    {
+        private SearchMetadata(DateTime searchTime)
+        {
+            SearchTime = searchTime;
+        }
+        public DateTime SearchTime { get; private set; }
+
+        internal static SearchMetadata NewInstance()
+        {
+            return new SearchMetadata(DateTime.Now);
+        }
+    }
+}
