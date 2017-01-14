0774d895f84b980dfc681256ed24f9376eb90112
Cristina Troie <cristina.troie@anre.ro>
2017-01-14 16:21:57 +0000

    Use logging in individual phantomjs steps

--- NavigationSteps/NavigationSteps.cs
+++ NavigationSteps/NavigationSteps.cs
@@ -1,23 +1,30 @@
 ﻿using OpenQA.Selenium;
 using PecoOnlineScraper.Helpers;
 
+using log4net;
+
 namespace PecoOnlineScraper.Steps
 {
     public class NavigationSteps
     {
 
+        private readonly ILog logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         private void ClickJudetRadio(IWebDriver driver)
         {
+            logger.Debug("Click 'judet' radio button");
             driver.FindElement(By.Id("Judet")).Click();
         }
 
         private void ClickGPLRadio(IWebDriver driver)
         {
+            logger.Debug("Click 'GPL' radio button");
             driver.FindElement(By.Id("GPL")).Click();
         }
 
         private void SetJudet(IWebDriver driver, string judet)
         {
+            logger.Debug(string.Format("Set {0} in search box", judet));
             IWebElement textbox = driver.FindElement(By.Id("nume_locatie"));
             textbox.Clear();
             textbox.SendKeys(judet);
@@ -25,12 +32,14 @@ namespace PecoOnlineScraper.Steps
 
         private void ClickSearch(IWebDriver driver)
         {
+            logger.Debug("Click search button");
             IWebElement button = driver.FindElement(By.Name("Submit"));
-            button.Click();           
+            button.Click();
         }
 
         private void WaitForResults(IWebDriver driver)
         {
+            logger.Debug("Wait for results to load");
             driver.WaitForElement(By.Id("tabelaRezultate"), 20);
         }
 
--- ResultsSearch/ResultsSearch.cs
+++ ResultsSearch/ResultsSearch.cs
@@ -3,17 +3,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
+using log4net;
+
 namespace PecoOnlineScraper.Results
 {
     public class ResultsSearch
     {
+
+        private readonly ILog logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         private IWebElement GetResultsTable(IWebDriver driver)
         {
+            logger.Debug("Load main results table");
             return driver.FindElement(By.Id("tabelaRezultate"));
         }
 
         private IEnumerable<IWebElement> GetPricesList(IWebElement table)
         {
+            logger.Debug("Load price elements");
             return table.FindElements(By.CssSelector(".pretTD"));
         }
 
